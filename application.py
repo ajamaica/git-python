16805ab3d21d2416a7070b33667f3d4e52e3822c
Arturo Jamaica <admin@example.com>
2017-04-30 21:00:12 -0500

    request done

--- application.py
+++ application.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+	url = 'https://api.github.com/search/repositories?q=%s' % ("")
+	r = requests.get(url)
+	return '%s' % r.json()
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=9876)
