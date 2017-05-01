687aebddbf1a95f81f37bb913f6416b4f1aabea7
Arturo Jamaica <admin@example.com>
2017-04-30 21:05:55 -0500

    Templates added

--- application.py
+++ application.py
@@ -1,11 +1,14 @@
 from flask import Flask
 import requests
+from flask import request
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-	url = 'https://api.github.com/search/repositories?q=%s' % ("")
+	search_term = request.args.get('search_term')
+	url = 'https://api.github.com/search/repositories?sort=updated&q=%s' % (search_term)
 	r = requests.get(url)
 	return '%s' % r.json()
 
